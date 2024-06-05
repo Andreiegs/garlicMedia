de1de5862fcf364c50dffdd15a9a716ebd430299
Andreiegs <andreielijahpracadio@gmail.com>
2024-06-05 21:25:41 +0800

    footer

--- src/pages/about.php
+++ src/pages/about.php
@@ -209,34 +209,11 @@
 		
 
 		<!-- Start Footer Section -->
-		<footer class="footer-section">
+		<footer class="footer-section mt-5">
 			<div class="container relative">
 
 				<div class="sofa-img">
 					<img src="../images/sofa.png" alt="Image" class="img-fluid">
-				</div>
-
-				<div class="row">
-					<div class="col-lg-8">
-						<div class="subscription-form">
-							<h3 class="d-flex align-items-center"><span class="me-1"><img src="../images/envelope-outline.svg" alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
-
-							<form action="#" class="row g-3">
-								<div class="col-auto">
-									<input type="text" class="form-control" placeholder="Enter your name">
-								</div>
-								<div class="col-auto">
-									<input type="email" class="form-control" placeholder="Enter your email">
-								</div>
-								<div class="col-auto">
-									<button class="btn btn-primary">
-										<span class="fa fa-paper-plane"></span>
-									</button>
-								</div>
-							</form>
-
-						</div>
-					</div>
 				</div>
 
 				<div class="row g-5 mb-5">
@@ -257,6 +234,8 @@
 							<div class="col-6 col-sm-6 col-md-3">
 								<ul class="list-unstyled">
 									<li><a href="#">About us</a></li>
+									<li><a href="#">Services</a></li>
+									<li><a href="#">Blog</a></li>
 									<li><a href="#">Contact us</a></li>
 								</ul>
 							</div>
@@ -265,7 +244,7 @@
 								<ul class="list-unstyled">
 									<li><a href="#">Support</a></li>
 									<li><a href="#">Knowledge base</a></li>
-									<li><a href="#">Live chat</a></li>
+									<li><a href="adminPage.php">Admin Page</a></li>
 								</ul>
 							</div>
 
@@ -278,13 +257,6 @@
 								</ul>
 							</div>
 
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">Nordic Chair</a></li>
-									<li><a href="#">Kruzo Aero</a></li>
-									<li><a href="#">Ergonomic Chair</a></li>
-								</ul>
-							</div>
 						</div>
 					</div>
 
@@ -293,7 +265,8 @@
 				<div class="border-top copyright">
 					<div class="row pt-4">
 						<div class="col-lg-6">
-							<p class="mb-2 text-center text-lg-start">Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. 
+							<p class="mb-2 text-center text-lg-start">Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> Distributed By <a hreff="https://themewagon.com">ThemeWagon</a>  <!-- License information: https://untree.co/license/ -->
+            </p>
 						</div>
 
 						<div class="col-lg-6 text-center text-lg-end">
@@ -309,7 +282,6 @@
 			</div>
 		</footer>
 		<!-- End Footer Section -->	
-
 
 		<script src="../js/bootstrap.bundle.min.js"></script>
 		<script src="../js/tiny-slider.js"></script>
